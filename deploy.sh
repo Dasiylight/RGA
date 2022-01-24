1123ffdaa9d2d75a2b8d5b8db864148b2f123c07
Dasiylight <zhiqinlong@foxmail.com>
2022-01-24 14:29:19 +0800

    fix

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,10 @@
-# 发生错误时终止
+# 发生任何错误时终止
 set -e
 
 # 构建
 npm run build
 
-# 进入构建文件夹
+# 进入输出产物文件夹
 cd dist
 
 # 如果你要部署到自定义域名
@@ -15,9 +15,9 @@ git add -A
 git commit -m 'deploy'
 
 # 如果你要部署在 https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 如果你要部署在 https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
+git push -f git@github.com:Wang9977/viteProject.git master:gh-pages
 
 cd -
